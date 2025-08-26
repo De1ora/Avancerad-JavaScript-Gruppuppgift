05e10a2b889373311e5c09a79b5c3f17ff656df3
AthirK <athirkazem@gmail.com>
2025-08-26 09:50:58 +0200

    blog started

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        npm install eslint
+      </h1>
+    </>
+  )
+}
+
+export default App
